46653ea5570dd349c7a012eb274b6fd05bd9c4a8
ryoi <ryoi.spl@gmail.com>
2014-12-31 01:51:50 +0900

    update auth

--- js/login.js
+++ js/login.js
@@ -1,11 +1,28 @@
+
+// http://qiita.com/Evolutor_web/items/449065d1bd82c51fef4c
+var setCookie = (function(){
+	return function(data, period) {
+		var cookies = '';
+		for (var key in data) {
+			cookies += key + '=' + encodeURIComponent(data[key]) + '; ';
+		}
+
+		var expire = new Date();
+		expire.setTime( expire.getTime() + 1000 * 3600 * 24 * period);
+		expire.toUTCString();
+
+		cookies += 'expires=' + expire+';';
+
+		document.cookie = cookies;
+	};
+})();
+
 $(function(){
 
 	$("#login").click(function(){
-		$.get("../only/auth.pl",  { passwd: $("#passwd").val() }, 
-			function(data){
-				alert("Data Loaded: " + data);
-			}
-		);
+		$.get("../only/auth.pl",  { passwd: $("#passwd").val() }, function(data){
+			setCookie({onlyhash:data}, '1h');
+		});
 	});
 
 });
