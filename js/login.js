4749284a2571351f92e4190a23aeacfdfbec78a6
root <root@ryoi.ex-cloud.org>
2015-01-06 14:22:28 +0900

    update auth script

--- js/login.js
+++ js/login.js
@@ -1,5 +1,7 @@
 
 // http://qiita.com/Evolutor_web/items/449065d1bd82c51fef4c
+// setCookie(data, period);
+// data: hash key = cookie name, hash value = cookie body
 // period: expire time(hour)
 var setCookie = (function(){
 	return function(data, period) {
@@ -21,11 +23,26 @@ var setCookie = (function(){
 $(function(){
 
 	$("#login").submit(function(){
+		if (!$("#error_message").length) {
+			$("article").append('<p id="error_message"></p>');
+		}
+
+		if ($("#passwd").val() == "") {
+			$("#error_message").html("パスワードを入力してください。");
+			return false;
+		}
+
 		$.get("../only/auth.pl",  { passwd: $("#passwd").val() }, function(data){
-			setCookie({onlyhash:data}, 1);
-			window.location.href = window.location.href;
-			return true;
+			var res = JSON.parse(data);
+			if (res.ok) {
+				setCookie({onlyhash:res.hash}, 1);
+				location.reload();
+			} else {
+				$("#error_message").html("パスワードが正しくありません。");
+			}
 		});
+
+		return false;
 	});
 
 });
--- only/auth.pl
+++ only/auth.pl
@@ -1,18 +1,26 @@
 #!/usr/bin/perl
 
-use lib qw(/home/ccs/public_html/new/only/lib);
-
 use strict;
 use warnings;
 
 use CGI;
-use Digest::MD5 qw(md5_hex);
+use JSON::PP;
+use Digest::SHA;
 
+my $PASSWORD = '36346c1532c5579654456183c1b2a95ac4e499e881b91e6c654686ab27fb1942da1aa4e7a1fceced2291e0c476d01e86c233c1aa3410ff72addf652289e1da45';
 my $SALT = '}_Y=cIaDV}J,kzZ@a4EA*bK~svx(.QyD';
+
+my $sha = Digest::SHA->new(512);
 
 my $query = new CGI;
 my $passwd = $query->param('passwd');
+my $hash = $sha->add($passwd.$SALT)->hexdigest;
+
+my %res;
+$res{'hash'} = $hash;
+$res{'ok'} = $PASSWORD eq $hash ? 1 : 0;
 
 print "Content-type:text/plain\n\n";
-print md5_hex($passwd.$SALT), "\n";
+my $json = encode_json \%res;
+print $json, "\n";
 
